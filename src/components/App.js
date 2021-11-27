7621a33b39dad3e73b9232e6af787fa6e5ed68ae
Pedro Ramos <pedrotr@hotmail.com>
2021-11-27 13:12:00 -0300

    rpouter set up

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,19 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { useMoralis } from 'react-moralis';
-import { Button, Flex, Heading } from '@chakra-ui/react';
+import Auth from './pages/Auth';
+import Profile from './pages/Profile';
+import Home from './pages/Home';
 
 const App = () => {
-  const { authenticate, isAuthenticated, logout } = useMoralis();
   return (
-    <Flex w='100%' p={4} align='center' justify='center' direction='column'>
-      {isAuthenticated && <Heading size='lg'>You're logged in.</Heading>}
-      <Button
-        m={4}
-        colorScheme='blue'
-        onClick={() => (!isAuthenticated ? authenticate() : logout())}
-      >
-        {!isAuthenticated ? ' Sign in with Metamask' : 'Logout'}
-      </Button>
-    </Flex>
+    <Router>
+      <Routes>
+        <Route path='/auth' element={<Auth />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+    </Router>
   );
 };
 
